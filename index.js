85c6bd8bd81df6764bc0ed761915780fda434bc3
Andrew Borisenko <seigiard@gmail.com>
2024-12-25 07:02:03 +0100

    save file

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+import fs from 'fs';
 import { generateEpgXml } from './src/generateEpgXml.js';
 
 const xml = await generateEpgXml();
-console.log(xml)
+
+const args = process.argv.slice(2);
+const [ file ]= args
+
+try {
+    fs.writeFileSync(file, xml, 'utf8');
+    console.log('File has been written successfully');
+} catch (err) {
+    console.error('Error writing file:', err);
+}
